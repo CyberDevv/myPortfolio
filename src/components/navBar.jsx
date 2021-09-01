0e3707ae3be290a02dbd30acc02542d5a1e28d96
CyberDevv Workspace <tocyerdevv@gmail.com>
2021-09-01 17:06:16 +0100

    Enable dynamic framer props on pagelink and sociallink

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -40,6 +40,13 @@ const NavBar = () => {
           styleClass="lg:flex space-x-12 items-center hidden justify-around capitalize tracking-widest text-light w-8/12 z-10 overflow-hidden"
           linkStyle="navItem py-1"
           activeStyleClass="text-primary font-semibold active"
+          initial={{ width: 0 }}
+          animate={{ width: "100%" }}
+          transition={{
+            type: "tween",
+            delay: 0.5,
+            duration: 2,
+          }}
         ></PageLinks>
 
         {/* mobile NavMenu */}
@@ -130,8 +137,23 @@ const NavBar = () => {
                   styleClass="flex flex-col justify-center items-center uppercase font-bold text-xl tracking-widest leading-10 space-y-8"
                   linkStyle=""
                   activeStyleClass="mobileActive relative"
+                  initial={{ x: "-100vw" }}
+                  animate={{ x: 0 }}
+                  transition={{
+                    delay: 0.5,
+                  }}
                 ></PageLinks>
-                <SocialLink styleClass="flex justify-evenly text-xl text-2xl w-full" />
+                <SocialLink
+                  styleClass="flex justify-evenly text-xl text-2xl w-full"
+                  initial={{ opacity: 0, y: -50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    delay: 1,
+                    stiffness: 50,
+                    damping: 5,
+                    type: "spring",
+                  }}
+                />
               </div>
             </motion.div>
           )}
