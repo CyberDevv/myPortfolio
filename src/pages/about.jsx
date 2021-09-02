e92fd2c9b60e9fd0679ce2dfec6edf514d3f72e6
CyberDevv <tocyberdevv@gmail.com>
2021-09-02 18:03:13 +0100

    Starting Skills section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import AboutMe from '../components/AboutMe'
-import HeaderTitles from '../components/HeaderTitles'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import React from "react"
+import AboutMe from "../components/AboutMe"
+import HeaderTitles from "../components/HeaderTitles"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const about = () => {
   return (
     <Layout>
       <Seo title="About" />
-      <HeaderTitles title="Know About Me" />
+      <HeaderTitles title="Know About Me" className="aboutTitle" />
       <AboutMe />
+      <HeaderTitles title="Skills" className="skillTitle mt-32" />
     </Layout>
   )
 }
