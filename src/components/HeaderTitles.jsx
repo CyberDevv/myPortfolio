15cc3068a5c23ba1975dfa93ad1b3d6c41479543
CyberDevv Workspace <tocyerdevv@gmail.com>
2021-09-02 16:55:22 +0100

    Made about page responsive

--- src/components/HeaderTitles.jsx
+++ src/components/HeaderTitles.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 const HeaderTitles = ({ title }) => {
   return (
     <motion.h1
-      className="text-5xl font-black headerTitle relative pt-2 uppercase leading-loose"
+      className="lg:text-5xl font-black headerTitle relative pt-2 uppercase leading-loose"
       initial={{ opacity: 0 }}
       animate= {{opacity: 1}}
       transition= {{delay: 2}}
