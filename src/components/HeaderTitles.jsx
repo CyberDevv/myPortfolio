46eaa76939c1ca12294d8deda448ad7c9e132859
CyberDevv <tocyberdevv@gmail.com>
2021-09-03 15:22:47 +0100

    added skill section

--- src/components/HeaderTitles.jsx
+++ src/components/HeaderTitles.jsx
@@ -1,16 +1,21 @@
 import React from "react"
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
-const HeaderTitles = ({ title, className }) => {
+const HeaderTitles = ({ title, className, transition }) => {
   return (
-    <motion.h1
-      className={` ${className ? className : "" } lg:text-5xl font-black headerTitle relative pt-2 uppercase leading-loose`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 2 }}
-    >
-      {title}
-    </motion.h1>
+    <AnimatePresence>
+      <motion.h1
+        className={` ${
+          className ? className : ""
+        } lg:text-5xl font-black headerTitle relative pt-2 uppercase leading-loose`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit= {{opacity: 0}}
+        transition={transition}
+      >
+        {title}
+      </motion.h1>
+    </AnimatePresence>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Skill from './Skill'
 
 const Skills = () => {
   return (
-    <div>
-      skills section
+    <div className="grid lg:grid-cols-2 gap-8 my-8">
+      <Skill skill="Html5" progress="97" className="w-97%" />
+      <Skill skill="CSS3" progress="95" className="w-95%" />
+      <Skill skill="Javascript" progress="90" className="w-90%" />
+      <Skill skill="TypeScript" progress="50" className="w-2/4" />
+      <Skill skill="TailwindCSS" progress="100" className="w-full" />
+      <Skill skill="NodeJS" progress="70" className="w-70%" />
+      <Skill skill="ReactJS" progress="87" className="w-87%" />
+      <Skill skill="GatsbyJS" progress="87" className="w-87%" />
+      <Skill skill="Git" progress="88" className="w-88%" />
+      <Skill skill="Communication" progress="90" className="w-90%" />
     </div>
   )
 }
