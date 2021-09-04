9cdec0f4e5b927f3880b2bbd0d4cc57525a22324
CyberDevv <tocyberdevv@gmail.com>
2021-09-04 13:15:14 +0100

    Made project page better

---
+++ src/components/Project.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import Img from 'gatsby-image'
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
+
+const Project = ({
+  Title,
+  URL,
+  Description,
+  SourceCodeLInk,
+  TechnologyUsed,
+  Placeholder,
+  index,
+}) => {
+
+  return (
+    <div className="project">
+      <Img
+        fluid={Placeholder.localFile.childImageSharp.fluid}
+        alt={Title}
+        className="project-img"
+      />
+      <div className="project-info py-4">
+        <h3 className="text-primary capiterlize tracking-wider">
+          {Title}
+        </h3>
+        <p className="project-desc">{Description}</p>
+        <div className="project-stack mt-4">
+          {TechnologyUsed.map(item => {
+            return <span key={item.id}>{item.input}</span>
+          })}
+        </div>
+        <div className="flex items-center">
+          <a
+            href={SourceCodeLInk}
+            className="project-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
+          <a
+            href={URL}
+            className="project-icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaShareSquare />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Project
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeaderTitles from '../components/HeaderTitles'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Project from '../components/Projects'
 
 const project = () => {
   return (
     <Layout>
       <Seo title="Projects" />
-      <HeaderTitles title="Projects" className="projectTitle" />
+      <HeaderTitles title="Projects" className="projectTitle mb-16" />
+      <Project />
     </Layout>
   )
 }
