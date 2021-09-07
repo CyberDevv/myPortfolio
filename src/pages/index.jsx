fc51820cf252e53b072edb0e3a161bb9ad09cbba
CyberDevv <tocyberdevv@gmail.com>
2021-09-07 17:13:26 +0100

    Added loader

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,127 +1,154 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Particles from "react-particles-js"
 import { motion } from "framer-motion"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Lottie from "react-lottie"
+import * as animationData from "../json/loader.json"
 
 const IndexPage = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  }
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
   return (
-    <Layout index>
-      <Seo
-        title="Home"
-        desc="Welcome to my portfolio where you can see some details about me and my social links and also some projects I've built."
-      />
-      <motion.div
-        className="md:hidden"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3 }}
-      >
-        <Particles
-          className="absolute top-0 left-0 h-screen w-full"
-          params={{
-            particles: {
-              number: {
-                value: 50,
-              },
-              size: {
-                value: 3,
-              },
-            },
-            fpsLimit: 60,
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "repulse",
-                  parallax: {
-                    enable: true,
-                    force: 60,
-                    smooth: 10,
+    <div>
+      {loading ? (
+        <div className= "fixed h-screen w-full">
+          <Lottie
+            options={defaultOptions}
+            ariaRole="div"
+            style={{ pointerEvents: "none" }}
+          />
+        </div>
+      ) : (
+        <Layout index>
+          <Seo
+            title="Home"
+            desc="Welcome to my portfolio where you can see some details about me and my social links and also some projects I've built."
+          />
+          <motion.div
+            className="md:hidden"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3 }}
+          >
+            <Particles
+              className="absolute top-0 left-0 h-screen w-full"
+              params={{
+                particles: {
+                  number: {
+                    value: 50,
+                  },
+                  size: {
+                    value: 3,
                   },
                 },
-              },
-            },
-            responsive: [],
-          }}
-        />
-      </motion.div>
-
-      <motion.div
-        className="hidden md:inline"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3 }}
-      >
-        <Particles
-          className="absolute top-0 left-0 h-screen w-full"
-          params={{
-            particles: {
-              number: {
-                value: 100,
-              },
-              size: {
-                value: 3,
-              },
-            },
-            fpsLimit: 60,
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "repulse",
-                  parallax: {
-                    enable: true,
-                    force: 60,
-                    smooth: 10,
+                fpsLimit: 60,
+                interactivity: {
+                  events: {
+                    onhover: {
+                      enable: true,
+                      mode: "repulse",
+                      parallax: {
+                        enable: true,
+                        force: 60,
+                        smooth: 10,
+                      },
+                    },
                   },
                 },
-              },
-            },
-            responsive: [],
-          }}
-        />
-      </motion.div>
+                responsive: [],
+              }}
+            />
+          </motion.div>
 
-      <motion.div
-        className="hidden 2xl:inline"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3 }}
-      >
-        <Particles
-          className="absolute top-0 left-0 h-screen w-full"
-          params={{
-            particles: {
-              number: {
-                value: 150,
-              },
-              size: {
-                value: 3,
-              },
-            },
-            fpsLimit: 60,
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "repulse",
-                  parallax: {
-                    enable: true,
-                    force: 60,
-                    smooth: 10,
+          <motion.div
+            className="hidden md:inline"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3 }}
+          >
+            <Particles
+              className="absolute top-0 left-0 h-screen w-full"
+              params={{
+                particles: {
+                  number: {
+                    value: 100,
+                  },
+                  size: {
+                    value: 3,
                   },
                 },
-              },
-            },
-            responsive: [],
-          }}
-        />
-      </motion.div>
+                fpsLimit: 60,
+                interactivity: {
+                  events: {
+                    onhover: {
+                      enable: true,
+                      mode: "repulse",
+                      parallax: {
+                        enable: true,
+                        force: 60,
+                        smooth: 10,
+                      },
+                    },
+                  },
+                },
+                responsive: [],
+              }}
+            />
+          </motion.div>
 
-      <Hero />
-    </Layout>
+          <motion.div
+            className="hidden 2xl:inline"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3 }}
+          >
+            <Particles
+              className="absolute top-0 left-0 h-screen w-full"
+              params={{
+                particles: {
+                  number: {
+                    value: 150,
+                  },
+                  size: {
+                    value: 3,
+                  },
+                },
+                fpsLimit: 60,
+                interactivity: {
+                  events: {
+                    onhover: {
+                      enable: true,
+                      mode: "repulse",
+                      parallax: {
+                        enable: true,
+                        force: 60,
+                        smooth: 10,
+                      },
+                    },
+                  },
+                },
+                responsive: [],
+              }}
+            />
+          </motion.div>
+
+          <Hero />
+        </Layout>
+      )}
+    </div>
   )
 }
 
