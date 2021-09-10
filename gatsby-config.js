bfb92c3113072fa4afd9160f19ec421c767d5bb8
CyberDevv <tocyberdevv@gmail.com>
2021-09-10 23:18:48 +0100

    Removed purgeCSS and google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@CyberDevv`,
     twitterUsername: `@CyberDevv`,
     image: `/myPortfolio.png`,
-    siteUrl: `https://ibrahim-odesola.netlify.app`,
+    siteUrl: `https://ibrahim-odesola.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -34,48 +34,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
-    },
-    {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-purgecss`,
-    //   options: {
-    //     printRejected: false,
-    //     develop: false,
-    //     tailwind: true,
-    //   },
-    // },
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
@@ -89,7 +52,7 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,
-        queryLimit: 1000,
+        queryLimit: 100,
         collectionTypes: [`projects`],
       },
     },
